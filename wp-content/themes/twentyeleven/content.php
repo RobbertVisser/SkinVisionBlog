3282246a110705098fdb1af68e313a735c80ecac
Mir <mirosul@gmail.com>
2012-11-02 03:40:17 +0200

    comment form

--- wp-content/themes/twentyeleven/content.php
+++ wp-content/themes/twentyeleven/content.php
@@ -67,10 +67,8 @@
 		<div class="entry-content">
 			<?php the_content( __( 'read more <span class="meta-nav">+</span>', 'twentyeleven' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+			<?php edit_post_link( __( 'Edit article', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
-		<footer class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- #entry-meta -->
 	</article><!-- #post-<?php the_ID(); ?> -->
