de1a96938e6a47d457048a73fc4f2159856e8c2a
Mir <mirosul@gmail.com>
2012-11-19 13:04:54 +0200

    skin-moles.com theme

---
+++ wp-content/themes/twentyeleven-two/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+    <div id="footer-wrapper">
+			<div id="site-generator">
+        2012 - &copy; skin-moles.com
+			</div>
+		</div>
+	</footer><!-- #colophon -->
+
+  <div id="backtotop">
+    <div>
+      <a href="#">
+        back to top
+      </a>
+    </div>
+  </div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
